7fc71ab29e212bb0b5a00fa4e0f17f792b6a4ccc
OBBoO <67312500+OBBoO@users.noreply.github.com>
2020-11-13 10:02:53 +0100

    Add files via upload

---
+++ Image to Text.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import pytesseract
+import re
+import xlsxwriter
+
+
+from pandas import ExcelWriter
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r#"executable path"
+
+img = #Image.open(Your Image)
+
+text = pytesseract.image_to_string(img)
+
+lines = text.splitlines( )
+
+for line in lines:
+    if line == ' ':
+        lines.remove(line)
+lines
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
